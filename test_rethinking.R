0b9408fd6d3f6810f1b47d81db978313b5159541
albertgoncalves <goncalves.albert@gmail.com>
2018-12-27 02:27:42 -0500

    Better example.

--- test_rethinking.R
+++ test_rethinking.R
@@ -3,13 +3,45 @@
 library(rethinking, lib.loc=getwd())
 
 if (sys.nframe() == 0) {
-    f = alist( y ~ dnorm(mu, sigma)
-             , mu ~ dnorm(0, 10)
-             , sigma ~ dcauchy(0, 1)
-             )
-    data = list(y=c(-1, 1))
-    start = list(mu=0, sigma=1)
+    options(mc.cores=parallel::detectCores())
 
-    fit = map(f, data=data, start=start)
-    print(fit)
+    data(chimpanzees)
+    d = chimpanzees
+    dat = list( y=d$pulled_left
+               , prosoc=d$prosoc_left
+               , condition=d$condition
+               , N=nrow(d)
+               )
+
+    m1s_code = "
+       data {
+           int<lower=1> N;
+           int y[N];
+           int prosoc[N];
+       }
+       parameters {
+           real a;
+           real bP;
+       }
+       model {
+           vector[N] p;
+           bP ~ normal(0, 1);
+           a ~ normal(0, 10);
+           for (i in 1:N)
+               p[i] = a + bP * prosoc[i];
+           y ~ binomial_logit(1, p);
+       }
+       generated quantities {
+           vector[N] p;
+           vector[N] log_lik;
+           for (i in 1:N) {
+               p[i] = a + bP * prosoc[i];
+               log_lik[i] = binomial_logit_log(y[i], 1, p[i]);
+           }
+       }
+    "
+
+    m1s = stan(model_code=m1s_code, data=dat, chains=2, iter=2000)
+    WAIC(m1s)
+    LOO(m1s)
 }
