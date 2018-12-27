2b2fa046b69c1a48665254f3614300ca088a0e05
albertgoncalves <goncalves.albert@gmail.com>
2018-12-27 14:28:08 -0500

    More.

--- ch2/2-4-2.R
+++ ch2/2-4-2.R
@@ -3,10 +3,23 @@
 library(rethinking, lib.loc=sprintf("%s/../", getwd()))
 
 if (sys.nframe() == 0) {
+    xlab = "proportion water"
+    ylab = "density"
+
+    ac = "analytical calculation"
+    w = 6
+    n = 9
+    curve(dbeta(x, w + 1, n - w + 1), from=0, to=1, xlab=xlab, ylab=ylab)
+
+    qa = "quadratic approximation"
     globe.qa = map( alist( w ~ dbinom(9, p) # binomial likelihood
                          , p ~ dunif(0, 1)  # uniform prior
                          )
                   , data=list(w=6)
                   )
+    cat(sprintf("\n%s\n", qa))
     print(precis(globe.qa))
+    curve(dnorm(x, 0.67, 0.16), lty=2, add=TRUE)
+
+    legend(0.05, 2.5, legend=c(ac, qa), lty=1:2, cex=0.8)
 }
