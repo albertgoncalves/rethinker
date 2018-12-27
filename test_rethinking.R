1597aaf7a0fcf19c5657fb68b25da70cd8cf0067
albertgoncalves <goncalves.albert@gmail.com>
2018-12-26 23:41:10 -0500

    Updates.

--- test_rethinking.R
+++ test_rethinking.R
@@ -7,9 +7,9 @@ if (sys.nframe() == 0) {
              , mu ~ dnorm(0, 10)
              , sigma ~ dcauchy(0, 1)
              )
-    fit = map( f
-             , data=list(y=c(-1, 1))
-             , start=list(mu=0, sigma=1)
-             )
+    data = list(y=c(-1, 1))
+    start = list(mu=0, sigma=1)
+
+    fit = map(f, data=data, start=start)
     print(fit)
 }
