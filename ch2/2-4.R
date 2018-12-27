e791b2dfcfcc89749df921eae6d1426058b6ec96
albertgoncalves <goncalves.albert@gmail.com>
2018-12-27 13:49:33 -0500

    First exercises.

---
+++ ch2/2-4.R
@@ -0,0 +1,26 @@
+#!/usr/bin/env Rscript
+
+posterior = function(p_grid, prior) {
+    likelihood = dbinom(6, size=9, prob=p_grid)
+    unstd.posterior = likelihood * prior
+    posterior = unstd.posterior / sum(unstd.posterior)
+    return(posterior)
+}
+
+if (sys.nframe() == 0) {
+    p_grid = seq(from =0, to=1, length.out=20)
+    priors = list( "rep(1, 20)"
+                 , "ifelse(p_grid < 0.5, 0, 1)"
+                 , "exp(-5 * abs(p_grid - 0.5))"
+                 )
+    for (prior in priors) {
+        plot( p_grid
+            , posterior(p_grid, eval(parse(text=prior)))
+            , type="b"
+            , xlab="probability of water"
+            , ylab="posterior probability"
+            )
+        title(main=sprintf("prior = %s", prior))
+        mtext("20 points")
+    }
+}
