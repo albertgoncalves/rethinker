e2ef810d8ecb9b25c98edc0df3d7fa7b08438ed3
albertgoncalves <goncalves.albert@gmail.com>
2018-12-30 17:20:49 -0500

    Slightly nicer.

--- ch4/4-1.R
+++ ch4/4-1.R
@@ -2,19 +2,20 @@
 
 library(rethinking, lib.loc=sprintf("%s/../", getwd()))
 
-add_norm = function(n) {
-    obs = replicate(1000, sum(runif(n, -1, 1)))
+add_norm = function(m, n) {
+    obs = replicate(m, sum(runif(n, -1, 1)))
     hist(obs, main=sprintf("n = %s", n))
     plot(density(obs), main=sprintf("n = %s", n))
 }
 
-multi_norm = function(n) return(replicate(10000, prod(1 + runif(12, 0, n))))
+multi_norm = function(m, n) return(replicate(m, prod(1 + runif(12, 0, n))))
 
-log_norm = function(n) return(replicate(10000, log(prod(1 + runif(12, 0, n)))))
+log_norm = function(m, n) return(replicate(m, log(prod(1 + runif(12, 0, n)))))
 
 if (sys.nframe() == 0) {
-    for (n in c(4, 8, 16)) add_norm(n)
-    for (growth in c(multi_norm, log_norm)) {
-        for (n in c(0.1, 5)) dens(growth(n), norm.comp=TRUE)
-    }
+    m = 10000
+    for (n in c(4, 8, 16)) add_norm(m, n)
+    for (f in c(multi_norm, log_norm))
+        for (n in c(0.1, 5))
+            dens(f(m, n), norm.comp=TRUE)
 }
