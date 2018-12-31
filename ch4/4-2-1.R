9af3edad5b9be98827f304017b78694c44441be5
albertgoncalves <goncalves.albert@gmail.com>
2018-12-31 18:58:49 -0500

    More things.

---
+++ ch4/4-2-1.R
@@ -0,0 +1,22 @@
+#!/usr/bin/env Rscript
+
+library(rethinking, lib.loc=sprintf("%s/../", getwd()))
+
+posterior_samples = function(p_grid, posterior, n) {
+    samples = sample(p_grid, prob=posterior, size=n, replace=TRUE)
+    return(samples)
+}
+
+if (sys.nframe() == 0) {
+    w = 6
+    n = 9
+    m = 10000
+    p_grid = seq(from=0, to=1, length.out=m)
+
+    # w ~ Binomial(n, p)
+    # p ~ Uniform(0, 1)
+    posterior = dbinom(w, n, p_grid) * dunif(p_grid, 0, 1)
+    samples = posterior_samples(p_grid, posterior / sum(posterior), m)
+    plot(samples)
+    dens(samples)
+}
---
+++ ch4/4-3.R
@@ -0,0 +1,65 @@
+#!/usr/bin/env Rscript
+
+library(rethinking, lib.loc=sprintf("%s/../", getwd()))
+
+if (sys.nframe() == 0) {
+    data(Howell1)
+    adults = Howell1[which(Howell1$age >= 18), ]
+    print(str(adults))
+    dens(adults$height)
+
+    mu_mu = 178
+    mu_sigma = 20
+    sigma_lower = 0
+    sigma_upper = 50
+    n = 10000
+
+    {
+        curve(dnorm(x, mu_mu, mu_sigma), from=100, to=250)
+        curve(dunif(x, sigma_lower, sigma_upper), from=-10, to=60)
+
+        mu = rnorm(n, mu_mu, mu_sigma)
+        sigma = runif(n, sigma_lower, sigma_upper)
+        prior = rnorm(n, mu, sigma)
+        dens(prior)
+    }
+
+    {
+        mu_list = seq(from=140, to=160, length.out=500)
+        sigma_list = seq(from=4, to=9, length.out=500)
+        post = expand.grid(mu=mu_list, sigma=sigma_list)
+        post$LL = sapply(1:nrow(post), function(i) return(
+            sum(dnorm( adults$height
+                     , mean=post$mu[i]
+                     , sd=post$sigma[i]
+                     , log=TRUE
+                     ))
+        ))
+        post$prod = post$LL
+            + dnorm(post$mu, mu_mu, mu_sigma, TRUE)
+            + dunif(post$sigma, sigma_lower, sigma_upper, TRUE)
+        post$prob = exp(post$prod - max(post$prod))
+
+        for (f in c(contour_xyz, image_xyz)) f(post$mu, post$sigma, post$prob)
+
+        sample_rows = sample( 1:nrow(post)
+                            , size=n
+                            , replace=TRUE
+                            , prob=post$prob
+                            )
+        sample_mu = post$mu[sample_rows]
+        sample_sigma = post$sigma[sample_rows]
+
+        plot( sample_mu
+            , sample_sigma
+            , cex=1.5
+            , pch=16
+            , col=col.alpha(rangi2, 0.1)
+            )
+
+        printHPDI = function(x) return(print(HPDI(x)))
+        for (s in list(sample_mu, sample_sigma))
+            for (f in list(dens, printHPDI))
+                f(s)
+    }
+}
