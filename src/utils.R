2b4f974c2ad32d8fa863f1c73ae29186442040fe
albertgoncalves <goncalves.albert@gmail.com>
2019-01-27 15:52:25 -0500

    Bugs.

--- src/utils.R
+++ src/utils.R
@@ -1,5 +1,9 @@
 #!/usr/bin/env Rscript
 
+inject = function(f, xs) {
+    return(do.call(deparse(substitute(f)), xs))
+}
+
 outline_2d = function(x, y) {
     m = matrix(c(x, y), ncol=2)
     ch = chull(m)
@@ -17,17 +21,17 @@ normalize = function(x, lower, upper) {
     return((x - lower) / (upper - lower))
 }
 
-color_ramp = function(colors, z, alpha=1, max=255) {
-    rgbs = colorRamp(plot_colors)(normalize(z, min(z), max(z)))
-    colors = rgb(rgbs[, 1], rgbs[, 2], rgbs[, 3], maxColorValue=max)
+color_ramp = function(colors, v, alpha=1, max=255) {
+    m = colorRamp(colors)(normalize(v, min(v), max(v)))
+    c = rgb(m[, 1], m[, 2], m[, 3], maxColorValue=max)
 
     if ((alpha > 1) | (alpha <= 0)) {
         stop("\n alpha out of bounds\n 0.0 < alpha <= 1.0")
     } else if (alpha < 1) {
-        colors = adjustcolor(colors, alpha.f=alpha)
+        c = adjustcolor(c, alpha.f=alpha)
     }
 
-    return(colors)
+    return(c)
 }
 
 if (sys.nframe() == 0) {
@@ -39,9 +43,12 @@ if (sys.nframe() == 0) {
     x = rn(n)
     y = rn(n)
 
-    plot_colors = c("lightsalmon2", "dodgerblue1", "green")
-    shade_color = adjustcolor("mediumvioletred", alpha.f=0.05)
+    kwargs =
+        list( colors=list("lightsalmon2", "dodgerblue1", "mediumseagreen")
+            , v=x
+            , alpha=0.85
+            )
 
-    plot(x, y, col=color_ramp(colors, x, alpha=0.85))
-    shade(x, y, col=shade_color)
+    plot(x, y, col=inject(color_ramp, kwargs))
+    shade(x, y, col=adjustcolor("mediumvioletred", alpha.f=0.05))
 }
